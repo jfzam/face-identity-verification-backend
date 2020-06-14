29529dd9117c681cf566789727fdaf7c44bd866a
unknown <frankln.zamora@gmail.com>
2020-06-14 15:53:30 +0800

    added dotenv

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const AWS = require('aws-sdk')
+AWS.config.update({region:'ap-southeast-1'})
+
+const bucket = 'jfzam-dev-rekognition'
+const photo_source  = 'frank.png'
+const photo_target  = 'jamsfree.jpg'
+
+
+
+const config = new AWS.Config({
+  accessKeyId: 'AKIA3J4GWA3WXXG6FC5V', //process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: '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', //process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-southeast-1" //process.env.AWS_REGION
+})
+
+
+
+const client = new AWS.Rekognition()
+
+const params = {
+  SourceImage: {
+    S3Object: {
+      Bucket: bucket,
+      Name: photo_source
+    },
+  },
+  TargetImage: {
+    S3Object: {
+      Bucket: bucket,
+      Name: photo_target
+    },
+  },
+  SimilarityThreshold: 80
+}
+
+client.compareFaces(params, function(err, response) {
+  if (err) {
+    console.log(err, err.stack); // an error occurred
+  } else {
+    response.FaceMatches.forEach(data => {
+      let position   = data.Face.BoundingBox
+      let similarity = data.Similarity
+      console.log(`The face at: ${position.Left}, ${position.Top} matches with ${similarity} % confidence`)
+    }) // for response.faceDetails
+  } // if
+})
